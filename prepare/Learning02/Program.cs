fe79a2c743cf2d3635dc61162a810f14871ce762
joakinleiva <joako.leiva9@gmail.com>
2023-03-06 02:23:07 -0300

    DONE

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -6,19 +6,19 @@ class Program
     {
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
-        job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
+        job1._company = "EA Sports";
+        job1._startYear = 2011;
+        job1._endYear = 2012;
 
         Job job2 = new Job();
-        job2._jobTitle = "Manager";
-        job2._company = "Apple";
-        job2._startYear = 2022;
+        job2._jobTitle = "Ceo";
+        job2._company = "Topside";
+        job2._startYear = 2021;
         job2._endYear = 2023;
 
         Resume resume1 = new Resume();
-        resume1._name = "Allison Rose";
-
+        resume1._name = "Joaquin Leiva";
+        
         resume1._jobs.Add(job1);
         resume1._jobs.Add(job2);
 
